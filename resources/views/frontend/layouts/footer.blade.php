be498d1d75beba1e4617d05e650d0c343c3cecaa
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-03-26 15:01:49 +0600

    Footer done

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -37,7 +37,8 @@
                     <div class="footer-widget widget-about aos-init" data-aos="fade-up" data-aos-duration="1500"
                         data-aos-offset="50">
                         <h6 class="footer-title text-white">About Company</h6>
-                        <p class="text-muted">Connect with us and stay updated on the latest news, offers, and updates.</p>
+                        <p class="text-muted">Connect with us and stay updated on the latest news, offers, and updates.
+                        </p>
                         <div class="social-style-one">
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -53,7 +54,7 @@
                         <ul>
                             <li><i class="fa fa-map-marker-alt"></i> 55 Main Street, 2nd block Melbourne, Australia
                             </li>
-                            <li><i class="fa fa-envelope"></i> <a href="mailto:support@gmail.com">support@gmail.com</a>
+                            <li><i class="fa fa-envelope"></i> <a href="mailto:support@gmail.com">support@goqr.mail.com</a>
                             </li>
                             <li><i class="fa fa-phone"></i> <a href="callto:+0001234455">+000 (123) 44 55</a></li>
                         </ul>
@@ -66,10 +67,9 @@
                                 data-aos-duration="1500" data-aos-offset="50">
                                 <h6 class="footer-title text-white">Resources</h6>
                                 <ul>
-                                    <li><a href="shop.html">Product</a></li>
-                                    <li><a href="{{route('services')}}">Services</a></li>
+                                    <li><a href="{{ route('services') }}">Services</a></li>
                                     <li><a href="{{ route('about') }}">About Us</a></li>
-                                    <li><a href="{{route('services')}}">Benefits</a></li>
+                                    <li><a href="{{ route('contact') }}">Contact Us</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -78,22 +78,20 @@
                                 data-aos-duration="1500" data-aos-offset="50">
                                 <h6 class="footer-title text-white">Quick Link</h6>
                                 <ul>
-                                    <li><a href="services.html">Features</a></li>
-                                    <li><a href="pricing.html">Pricing Plan</a></li>
-                                    <li><a href="about.html">Best Program</a></li>
-                                    <li><a href="contact.html">Press Kit</a></li>
+                                    <li><a href="pricing.html">Pricing</a></li>
+                                    <li><a href="pricing.html">QR Code</a></li>
+                                    <li><a href="about.html">Business Card</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-sm-4 col-6">
                             <div class="footer-widget widget-links aos-init" data-aos="fade-up" data-aos-delay="300"
                                 data-aos-duration="1500" data-aos-offset="50">
-                                <h6 class="footer-title text-white">Company</h6>
+                                <h6 class="footer-title text-white">Condition</h6>
                                 <ul>
-                                    <li><a href="{{ route('about') }}">About</a></li>
-                                    <li><a href="team.html">Team Member</a></li>
-                                    <li><a href="about.html">Reviews</a></li>
-                                    <li><a href="blog.html">Latest News</a></li>
+                                    <li><a href="{{ route('about') }}">Terms</a></li>
+                                    <li><a href="team.html">Policy</a></li>
+                                    <li><a href="team.html">Faq</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -102,27 +100,16 @@
             </div>
             <div class="footer-bottom mt-30 py-15">
                 <div class="row align-items-center">
-                    <div class="col-xl-4 col-lg-6">
+                    <div class="col-xl-12 col-lg-6">
                         <div class="copyright-text pt-10 text-lg-start text-center aos-init" data-aos="fade-left"
                             data-aos-duration="1000" data-aos-offset="50">
                             <p>Copyright @2024, <a href="index.html">Go QR </a> All Rights Reserved</p>
                         </div>
                     </div>
-                    <div class="col-xl-8 col-lg-6">
-                        <div class="footer-bottom-menu text-lg-end text-center aos-init" data-aos="fade-right"
-                            data-aos-duration="1000" data-aos-offset="50">
-                            <ul class="d-flex justify-content-end ">
-                                <li class="pe-3"><a href="faqs.html">Faqs</a></li>
-                                <li class="pe-3"><a href="about.html">Setting</a></li>
-                                <li class="pe-3"><a href="about.html">Privacy</a></li>
-                                <li class="pe-3"><a href="{{ route('contact') }}">Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
                 <!-- Scroll Top Button -->
-                <button class="scroll-top scroll-to-target me-3" data-target="html" style="display: inline-block;"><span
-                        class="fas fa-angle-double-up"></span></button>
+                <button class="scroll-top scroll-to-target me-3" data-target="html"
+                    style="display: inline-block;"><span class="fas fa-angle-double-up"></span></button>
             </div>
         </div>
     </div>
