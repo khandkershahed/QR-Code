06787c7a54d55c47991801df7829bcf92e1a5d7e
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-03-26 16:09:42 +0600

    page build done with content

---
+++ resources/views/frontend/pages/terms.blade.php
@@ -0,0 +1,54 @@
+<x-frontend-app-layout :title="'Terms'">
+    <section class="page-banner-area overlay py-250 rpy-120 rel z-1 bgs-cover text-center"
+        style="
+      background-image: url(https://webtendtheme.net/html/2024/akpager/assets/images/backgrounds/banner.jpg);
+    ">
+        <div class="container">
+            <div class="banner-inner pt-70 rpt-60 text-black">
+                <h1 class="page-title aos-init aos-animate" data-aos="fade-up" data-aos-duration="1500"
+                    data-aos-offset="50">
+                    Our Terms
+                </h1>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 pt-30 pb-30">
+                    <h4>Terms of Service</h4>
+
+                    <p>Welcome to the Go QR Code website. Our platform provides comprehensive QR code generation and
+                        management solutions tailored to meet your diverse needs. Whether you're a business owner,
+                        marketer, or individual user, our services empower you to create, customize, and track QR codes
+                        effortlessly. With a user-friendly interface and robust features, Go QR Code simplifies the
+                        process of incorporating QR codes into your marketing campaigns, product packaging, or personal
+                        projects.</p>
+
+                    <h6>User Agreement</h6>
+                    <p>By accessing and using our website, you agree to comply with these Terms of Service. If you do
+                        not agree with any part of these terms, please do not use our services. Violation of these terms
+                        may result in account suspension or termination.</p>
+
+                    <h6>Acceptable Use Policy</h6>
+                    <p>Our Acceptable Use Policy outlines the rules and guidelines for using our platform. Users are
+                        prohibited from engaging in any illegal, harmful, or abusive activities while using our
+                        services. This includes but is not limited to spamming, distributing malware, or infringing upon
+                        the rights of others.</p>
+
+                    <h6>Privacy Policy</h6>
+                    <p>Your privacy is important to us. Our Privacy Policy explains how we collect, use, and protect
+                        your personal information when you use our website. We are committed to safeguarding your data
+                        and ensuring its confidentiality. We do not sell or share your information with third parties
+                        without your consent.</p>
+
+                    <footer>
+                        <p>&copy; 2024 Go QR Code. All rights reserved.</p>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </section>
+    @push('scripts')
+    @endpush
+</x-frontend-app-layout>
