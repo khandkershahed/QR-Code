2125dffdbc4b45e1c857adf5d2f7765d8ff8a965
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-04-01 23:08:33 +0600

    NFC Form Updated

--- resources/views/user/pages/nfc-card/partials/nfc_template.blade.php
+++ resources/views/user/pages/nfc-card/partials/nfc_template.blade.php
@@ -1,49 +1,53 @@
 <div class="row">
     <div class="col-lg-2">
-        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-1" checked>
+        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-1">
         <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
+            style="    height: 350px;
+    object-fit: contain;
+    padding: 0px !important;
+    display: flex;
+    justify-content: center;"
             for="type-1">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
+            <img class="" height="300px" src="https://i.ibb.co/MRz9sPh/1.png" alt="">
         </label>
         <!--end::Option-->
     </div>
     <div class="col-lg-2">
         <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-2">
         <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
+            style="    height: 350px;
+    object-fit: contain;
+    padding: 0px !important;
+    display: flex;
+    justify-content: center;"
             for="type-2">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
+            <img class="" height="300px" src="https://i.ibb.co/Zd95dHZ/2.png" alt="">
         </label>
         <!--end::Option-->
     </div>
     <div class="col-lg-2">
-        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-3">
+        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-3" checked>
         <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
+            style="    height: 350px;
+    object-fit: contain;
+    padding: 0px !important;
+    display: flex;
+    justify-content: center;"
             for="type-3">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
+            <img class="" height="300px" src="https://i.ibb.co/268574S/3.png" alt="">
         </label>
         <!--end::Option-->
     </div>
     <div class="col-lg-2">
         <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-4">
         <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
+            style="    height: 350px;
+    object-fit: contain;
+    padding: 0px !important;
+    display: flex;
+    justify-content: center;"
             for="type-4">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
-        </label>
-        <!--end::Option-->
-    </div>
-    <div class="col-lg-2">
-        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-6">
-        <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
-            for="type-6">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
-        </label>
-        <!--end::Option-->
-    </div>
-    <div class="col-lg-2">
-        <input type="radio" class="btn-check" name="qr_type" value="pdf" id="type-7">
-        <label class="btn btn-outline btn-outline-dashed btn-outline-default p-7 d-flex align-items-center mb-5"
-            for="type-7">
-            <img class="img-fluid" src="https://i.ibb.co/Xk793wv/Screenshot-1.png" alt="">
+            <img class="" height="300px" src="https://i.ibb.co/DQYn5Zj/4.png" alt="">
         </label>
         <!--end::Option-->
     </div>
