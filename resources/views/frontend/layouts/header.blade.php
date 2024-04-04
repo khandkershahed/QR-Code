2151e25f824427579a862eba8749d59483b586e2
shahed141123 <khandkershahed23@gmail.com>
2024-04-05 00:30:32 +0600

    04-5-24

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -56,7 +56,7 @@
                 <!-- Menu Button -->
                 <div class="menu-btns ms-lg-auto">
                     @auth
-                        <a href="{{ route('dashboard') }}" class="light-btn">My Dashboard</a>
+                        <a href="{{ route('dashboard') }}" class="light-btn"><i class="fas fa-user me-3"></i> My Dashboard</a>
                     @else
                         <a href="{{ route('register') }}" class="light-btn">Sign Up</a>
                     @endauth
