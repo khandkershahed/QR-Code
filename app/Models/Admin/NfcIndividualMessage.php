4520697ccdf6534bebdc631a590db1333c1fe81e
shahed141123 <khandkershahed23@gmail.com>
2024-04-28 23:58:08 +0600

    26 april

---
+++ app/Models/Admin/NfcIndividualMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NfcIndividualMessage extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
--- resources/views/frontend/pages/nfc/template_two.blade.php
+++ resources/views/frontend/pages/nfc/template_two.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>{{ optional($nfc_card->nfcData)->first_name }} {{ optional($nfc_card->nfcData)->last_name }}/title>
+    <title>{{ optional($nfc_card->nfcData)->first_name }} {{ optional($nfc_card->nfcData)->last_name }}</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
