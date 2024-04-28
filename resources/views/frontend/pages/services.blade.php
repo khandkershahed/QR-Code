c1211b6f4f9fdc10f1ea45920d213ffb0472b30b
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-04-28 11:50:10 +0600

    some changes Done

--- resources/views/frontend/pages/services.blade.php
+++ resources/views/frontend/pages/services.blade.php
@@ -226,100 +226,99 @@
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-xl-4 col-md-6 col-sm-10" data-aos="fade-up" data-aos-duration="1500"
-                    data-aos-offset="50">
-                    <div class="pricing-item style-five">
-                        <div class="title-price">
-                            <h5 class="title">Regular</h5>
-                            <div class="price">
-                                <span class="prev">$</span>15.<span class="next">/m</span>
-                            </div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6 col-lg-9 col-md-11">
+                        <div class="section-title text-center mb-45 aos-init aos-animate" data-aos="fade-up"
+                            data-aos-duration="1500" data-aos-offset="50">
+                            <h2>Explore Our Pricing Package and choose your plan</h2>
                         </div>
-                        <hr />
-                        <div class="text">
-                            Data curation involve the careful election organization, and
-                            maintenance
-                        </div>
-                        <ul class="icon-list">
+                    </div>
+                    <div class="col-lg-7 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="50"
+                        data-aos-duration="1500" data-aos-offset="50">
+                        <span class="save-percent"
+                            style="background-image: url({{ asset('frontend/assets/images/shapes/title-shape.png') }});">Save
+                            40%</span>
+                        <ul class="nav pricing-tab mb-55" role="tablist">
                             <li>
-                                <i class="fa-regular fa-circle-check"></i> 2 Limited sites available
+                                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#monthly">Monthly</button>
                             </li>
-                            <li><i class="fa-regular fa-circle-check"></i> 1 GB storage per site</li>
-                            <li><i class="fa-regular fa-circle-check"></i> Up to 5 pages per site</li>
-                            <li class="hide">
-                                <i class="fa-regular fa-circle-check"></i> Free SSL for custom domain
-                            </li>
-                            <li class="hide">
-                                <i class="fa-regular fa-circle-check"></i> Connect custom domain
+                            <li>
+                                <button class="nav-link active" data-bs-toggle="tab"
+                                    data-bs-target="#yearly">Yearly</button>
                             </li>
                         </ul>
-                        <a href="#" class="theme-btn style-two">Choose Package <i
-                                class="fas fa-arrow-right"></i></a>
-                        <div class="footer-text">No credit card required</div>
                     </div>
                 </div>
-                <div class="col-xl-4 col-md-6 col-sm-10" data-aos="fade-up" data-aos-delay="100"
-                    data-aos-duration="1500" data-aos-offset="50">
-                    <div class="pricing-item style-five">
-                        <div class="title-price">
-                            <h5 class="title color-two">Standard</h5>
-                            <div class="price">
-                                <span class="prev">$</span>35.<span class="next">/m</span>
-                            </div>
+                <div class="tab-content">
+                    <div class="tab-pane fade" id="monthly">
+                        <div class="row justify-content-center">
+                            @foreach ($monthly_plans as $monthly_plan)
+                                <div class="col-xl-4 col-md-6 col-sm-10 aos-init aos-animate" data-aos="fade-up"
+                                    data-aos-duration="1500" data-aos-offset="50">
+                                    <div class="pricing-item style-three">
+                                        <div class="circle"><img
+                                                src="{{ asset('frontend/assets/images/shapes/price-circle.png') }}"
+                                                alt="Shape">
+                                        </div>
+                                        <div class="price"><span
+                                                class="prev fs-2 me-2">{{ $monthly_plan->currency }}</span>{{ $monthly_plan->price }}<span
+                                                class="next">/month</span>
+                                        </div>
+                                        <div class="text">10 Qr Code For One Month</div>
+                                        <a href="{{ route('user.subscribe.post', $monthly_plan->slug) }}"
+                                            class="theme-btn">Choose Package <i class="fas fa-arrow-right"></i></a>
+                                        <h4 class="title">{{ $monthly_plan->title }}</h4>
+                                        <ul class="list">
+                                            @if (!empty($monthly_plan->descriptions))
+                                                @foreach (json_decode($monthly_plan->descriptions ?? '[]') as $description)
+                                                    <li>{{ $description }}</li>
+                                                @endforeach
+                                            @endif
+                                        </ul>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
-                        <hr />
-                        <div class="text">
-                            Data curation involve the careful election organization, and
-                            maintenance
+                    </div>
+                    <div class="tab-pane fade yearly show active" id="yearly">
+                        <div class="row justify-content-center">
+                            @foreach ($yearly_plans as $yearly_plan)
+                                <div class="col-xl-4 col-md-6 col-sm-10 aos-init aos-animate" data-aos="fade-up"
+                                    data-aos-duration="1500" data-aos-offset="50">
+                                    <div class="pricing-item style-three">
+                                        <div class="circle"><img
+                                                src="{{ asset('frontend/assets/images/shapes/price-circle.png') }}"
+                                                alt="Shape">
+                                        </div>
+                                        <div class="price"><span
+                                                class="prev fs-2 me-2">{{ $yearly_plan->currency }}</span>{{ $yearly_plan->price }}<span
+                                                class="next">/month</span>
+                                        </div>
+                                        <div class="text">5 Business Card</div>
+                                        <a href="{{ route('user.subscribe.post', $yearly_plan->slug) }}"
+                                            class="theme-btn">Choose Package <i class="fas fa-arrow-right"></i></a>
+                                        <h4 class="title">{{ $yearly_plan->title }}</h4>
+                                        <ul class="list">
+                                            @if (!empty($yearly_plan->descriptions))
+                                                @foreach (json_decode($yearly_plan->descriptions ?? '[]') as $description)
+                                                    <li>{{ $description }}</li>
+                                                @endforeach
+                                            @endif
+                                        </ul>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
-                        <ul class="icon-list">
-                            <li>
-                                <i class="fa-regular fa-circle-check"></i> 2 Limited sites available
-                            </li>
-                            <li><i class="fa-regular fa-circle-check"></i> 1 GB storage per site</li>
-                            <li><i class="fa-regular fa-circle-check"></i> Up to 5 pages per site</li>
-                            <li>
-                                <i class="fa-regular fa-circle-check"></i> Free SSL for custom domain
-                            </li>
-                            <li class="hide">
-                                <i class="fa-regular fa-circle-check"></i> Connect custom domain
-                            </li>
-                        </ul>
-                        <a href="#" class="theme-btn style-two">Choose Package <i
-                                class="fas fa-arrow-right"></i></a>
-                        <div class="footer-text">No credit card required</div>
                     </div>
                 </div>
-                <div class="col-xl-4 col-md-6 col-sm-10" data-aos="fade-up" data-aos-delay="200"
-                    data-aos-duration="1500" data-aos-offset="50">
-                    <div class="pricing-item style-five">
-                        <div class="title-price">
-                            <h5 class="title color-four">Diamond</h5>
-                            <div class="price">
-                                <span class="prev">$</span>98.<span class="next">/m</span>
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="text">
-                            Data curation involve the careful election organization, and
-                            maintenance
-                        </div>
-                        <ul class="icon-list">
-                            <li>
-                                <i class="fa-regular fa-circle-check"></i> 2 Limited sites available
-                            </li>
-                            <li><i class="fa-regular fa-circle-check"></i> 1 GB storage per site</li>
-                            <li><i class="fa-regular fa-circle-check"></i> Up to 5 pages per site</li>
-                            <li>
-                                <i class="fa-regular fa-circle-check"></i> Free SSL for custom domain
-                            </li>
-                            <li><i class="fa-regular fa-circle-check"></i> Connect custom domain</li>
-                        </ul>
-                        <a href="#" class="theme-btn style-two">Choose Package <i
-                                class="fas fa-arrow-right"></i></a>
-                        <div class="footer-text">No credit card required</div>
-                    </div>
+            </div>
+            <div class="pricing-three-shapes">
+                <div class="shape one">
+                    <img src="https://webtendtheme.net/html/2024/akpager/assets/images/shapes/price1.png" alt="Shape">
+                </div>
+                <div class="shape two">
+                    <img src="https://webtendtheme.net/html/2024/akpager/assets/images/shapes/price2.png" alt="Shape">
                 </div>
             </div>
         </div>
