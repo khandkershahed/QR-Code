4f57348dd9941aa5815e2dbecc015507051175d8
shahed141123 <khandkershahed23@gmail.com>
2024-05-05 23:37:33 +0600

    26 april

--- resources/views/user/pages/qr-code/partials/customize.blade.php
+++ resources/views/user/pages/qr-code/partials/customize.blade.php
@@ -10,7 +10,7 @@
                 <span class="nav-text text-gray-800 fw-bold fs-6 lh-1">Templates</span>
                 <span class="bullet-custom position-absolute bottom-0 w-100 h-4px bg-primary"></span>
             </a>
-        </li> --}} 
+        </li> --}}
         <li class="nav-item mb-3 pe-3 pe-lg-10" role="presentation">
             <a class="nav-link btn btn-outline btn-flex btn-color-muted btn-active-color-primary flex-column overflow-hidden w-100px h-85px p-0
                 active"
@@ -328,7 +328,7 @@
                     ],
                     [
                         'id' => 322,
-                        'value' => 'square',
+                        'value' => 'circle',
                         'image' => 'https://static.beaconstac.com/assets/img/mobstac-awesome-qr/eye-balls/rounded.svg',
                     ],
                     [
