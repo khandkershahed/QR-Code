e7493396991a78bf792f8fb17c25bd82f80204df
shahed141123 <khandkershahed23@gmail.com>
2024-05-06 23:13:46 +0600

    26 april

--- app/Http/Controllers/Admin/QrCodeController.php
+++ app/Http/Controllers/Admin/QrCodeController.php
@@ -357,7 +357,7 @@ class QrCodeController extends Controller
 
 
         // Return the URL of the generated QR code image
-        if ($qrCodeString) {
+        if ($qr->wasRecentlyCreated) {
             return redirect()->route('user.qr-code.index')->with('success', 'You have successfully generated QR Code.');
         } else {
             return redirect()->back()->with('error', 'Failed to generate QR code.');
