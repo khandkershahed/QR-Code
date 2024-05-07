1017bf180d11a45c17a5af6de543433d55841101
dev4ngenit <dev4.ngenit@gmail.com>
2024-05-07 11:40:46 +0600

    06-05-24:Shahed-10.22

--- app/Http/Controllers/Admin/QrCodeController.php
+++ app/Http/Controllers/Admin/QrCodeController.php
@@ -59,7 +59,7 @@ class QrCodeController extends Controller
             'ip_address' => $request->ip(),
         ]);
         if (!empty($qr)) {
-            dd($qr->qr_type);
+            // dd($qr->qr_type);
             if (!empty($qr->qrData->qr_data_website_url)) {
                 return redirect()->to($qr->qrData->qr_data_website_url);
             } elseif (!empty($qr->qrData->qr_data_call_number)) {
