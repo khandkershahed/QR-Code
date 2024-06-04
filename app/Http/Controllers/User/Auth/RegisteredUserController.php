9ba98da88cb40788373e318cb43617955fd24850
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-06-04 22:34:54 +0600

    plan page update

--- app/Http/Controllers/User/Auth/RegisteredUserController.php
+++ app/Http/Controllers/User/Auth/RegisteredUserController.php
@@ -85,6 +85,7 @@ class RegisteredUserController extends Controller
     {
         // Retrieve registration data from session
         $data = session()->get('registration_data');
+        dd($data);
         $plan = Plan::find($data['plan']);
         // Validate registration data
         $validatedData = Validator::make($data, [
