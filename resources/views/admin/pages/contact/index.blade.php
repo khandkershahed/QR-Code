9c7a6e734fca7f531a113ccc74bb4321aaef9e05
shahed141123 <khandkershahed23@gmail.com>
2024-06-13 17:22:28 +0600

    13-06-24

--- resources/views/admin/pages/contact/index.blade.php
+++ resources/views/admin/pages/contact/index.blade.php
@@ -29,7 +29,7 @@
                         <!--end::Search-->
                     </div>
                     <!--end::Card title-->
-                    
+
                 </div>
                 <!--end::Card header-->
                 <!--begin::Card body-->
@@ -42,18 +42,18 @@
                             <thead>
                                 <!--begin::Table row-->
                                 <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
-                                    <th class="w-10px pe-2">
+                                    {{-- <th class="w-10px pe-2">
                                         <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
                                             <input class="form-check-input" type="checkbox" data-kt-check="true"
                                                 data-kt-check-target="#kt_message_table .form-check-input"
                                                 value="1" />
                                         </div>
-                                    </th>
-                                    <th class="min-w-150px">Sl</th>
-                                    <th class="min-w-150px">Name</th>
-                                    <th class="min-w-150px">Email</th>
-                                    <th class="min-w-150px">Status</th>
-                                    <th class="text-end min-w-70px">Action</th>
+                                    </th> --}}
+                                    <th width="5%" class="text-center">Sl</th>
+                                    <th width="15%" class="text-center">Name</th>
+                                    <th width="20%" class="text-center">Email</th>
+                                    <th width="5%" class="text-center">Message</th>
+                                    <th width="10%" class="text-center min-w-70px">Action</th>
                                 </tr>
                                 <!--end::Table row-->
                             </thead>
@@ -64,11 +64,11 @@
                                     <!--begin::Table row-->
                                     <tr>
                                         <!--begin::Checkbox-->
-                                        <td>
+                                        {{-- <td>
                                             <div class="form-check form-check-sm form-check-custom form-check-solid">
                                                 <input class="form-check-input" type="checkbox" value="1" />
                                             </div>
-                                        </td>
+                                        </td> --}}
                                         <!--end::Checkbox-->
                                         <td>
                                             <!--begin::Sl-->
@@ -82,19 +82,13 @@
                                         </td>
                                         <td>
                                             <!--begin::Sl-->
-                                            <span class="fw-bolder"> {{ $contact->slug }}</span>
+                                            <span class="fw-bolder"> {{ $contact->email }}</span>
                                             <!--end::Sl-->
                                         </td>
                                         <td>
-                                            <!--begin::Status-->
-                                            <div
-                                                class="badge {{ $contact->status == 1 ? 'badge-light-success' : 'badge-light-danger' }}">
-                                                {{ $contact->status == 1 ? 'Active' : 'InActive' }}
-                                            </div>
-                                            <!--end::Status-->
-                                        </td>
-                                        <td>
-
+                                            <!--begin::Sl-->
+                                            <span class="fw-bolder"> {{ $contact->message }}</span>
+                                            <!--end::Sl-->
                                         </td>
                                         <!--begin::Action=-->
                                         <td class="text-end">
