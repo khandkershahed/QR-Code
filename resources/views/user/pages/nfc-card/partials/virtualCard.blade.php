5c3d4618d330734afda7f4f4232117f6ff801dff
Sazeduzzaman <sazeduzzaman@yahoo.com>
2024-06-14 10:01:52 +0600

    Some Changes Done

---
+++ resources/views/user/pages/nfc-card/partials/virtualCard.blade.php
@@ -0,0 +1,26 @@
+<x-nfc-card.template :templates="[
+    [
+        'value' => 'template-one',
+        'image' => 'frontend/assets/images/nfc-templates/template_one.jpg',
+    ],
+    [
+        'value' => 'template-two',
+        'image' => 'frontend/assets/images/nfc-templates/template_two.jpg',
+    ],
+    [
+        'value' => 'template-three',
+        'image' => 'frontend/assets/images/nfc-templates/template_three.jpg',
+    ],
+    [
+        'value' => 'template-four',
+        'image' => 'frontend/assets/images/nfc-templates/template_four.jpg',
+    ],
+    [
+        'value' => 'template-five',
+        'image' => 'frontend/assets/images/nfc-templates/template_five.jpg',
+    ],
+    [
+        'value' => 'template-six',
+        'image' => 'frontend/assets/images/nfc-templates/template_six.jpg',
+    ],
+]" />
