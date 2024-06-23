a8d9bbe30f5cd5f4fb4ce2efd5d9d8af9e051d9f
shahed141123 <khandkershahed23@gmail.com>
2024-06-23 16:40:18 +0600

    13-06-24

--- resources/views/admin/pages/qr-code/index.blade.php
+++ resources/views/admin/pages/qr-code/index.blade.php
@@ -202,13 +202,19 @@
 
                 // Private functions
                 var initDatatable = function() {
+                    // Check if the DataTable is already initialized
+                    if ($.fn.DataTable.isDataTable(table)) {
+                        // Destroy the existing instance
+                        $(table).DataTable().destroy();
+                    }
+
                     // Set date data order
                     const tableRows = table.querySelectorAll('tbody tr');
 
                     tableRows.forEach(row => {
                         const dateRow = row.querySelectorAll('td');
                         const realDate = moment(dateRow[3].innerHTML, "DD MMM YYYY, LT")
-                            .format(); // select date from 4th column in table
+                    .format(); // select date from 4th column in table
                         dateRow[3].setAttribute('data-order', realDate);
                     });
 
@@ -217,47 +223,6 @@
                         "info": false,
                         'order': [],
                         'pageLength': 10,
-                    });
-                }
-
-                // Hook export buttons
-                var exportButtons = () => {
-                    const documentTitle = 'Customer Orders Report';
-                    var buttons = new $.fn.dataTable.Buttons(table, {
-                        buttons: [{
-                                extend: 'copyHtml5',
-                                title: documentTitle
-                            },
-                            {
-                                extend: 'excelHtml5',
-                                title: documentTitle
-                            },
-                            {
-                                extend: 'csvHtml5',
-                                title: documentTitle
-                            },
-                            {
-                                extend: 'pdfHtml5',
-                                title: documentTitle
-                            }
-                        ]
-                    }).container().appendTo($('#qr_code_admin_buttons'));
-
-                    // Hook dropdown menu click event to datatable export buttons
-                    const exportButtons = document.querySelectorAll(
-                        '#qr_code_admin_export_menu [data-kt-export]');
-                    exportButtons.forEach(exportButton => {
-                        exportButton.addEventListener('click', e => {
-                            e.preventDefault();
-
-                            // Get clicked export value
-                            const exportValue = e.target.getAttribute('data-kt-export');
-                            const target = document.querySelector('.dt-buttons .buttons-' +
-                                exportValue);
-
-                            // Trigger click event on hidden datatable export buttons
-                            target.click();
-                        });
                     });
                 }
 
