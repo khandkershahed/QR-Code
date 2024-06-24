f2e87a3029baa5b618e11b60d0456d599226f2af
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-06-25 02:17:00 +0600

    19-06-24

--- resources/views/frontend/pages/nfc/template_three.blade.php
+++ resources/views/frontend/pages/nfc/template_three.blade.php
@@ -904,7 +904,7 @@
 
                 vcfContent += "END:VCARD";
 
-                if (isMobile) {
+                if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
                     // For mobile devices, open the data URI directly to prompt adding contact
                     var encodedVcfContent = encodeURIComponent(vcfContent);
                     var uri = 'data:text/vcard;charset=utf-8,' + encodedVcfContent;
