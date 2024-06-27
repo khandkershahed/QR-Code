a80918b219d8b67a85cab1468c11d8dddc638202
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-06-28 01:40:39 +0600

    qr preview

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -42,7 +42,7 @@
 
 
     @props(['title'])
-    <title>{{ $title ?? config('app.name', 'GO-QR') }}</title>
+    <title>{{ $title || 'Your Hub for QR, Bar Codes, NFC V.Cards' ?? config('app.name', 'GO-QR || Your Hub for QR, Bar Codes, NFC V.Cards') }}</title>
 </head>
 
 <body id="kt_body"
