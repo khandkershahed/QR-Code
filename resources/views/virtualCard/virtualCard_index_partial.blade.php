ddd8ffd65f2392813198106f34e8a656db710174
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-07-18 13:22:15 +0600

    Some Change Done

--- resources/views/virtualCard/virtualCard_index_partial.blade.php
+++ resources/views/virtualCard/virtualCard_index_partial.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-12 mt-10">
+<div class="col-lg-12 mt-10 pb-10">
     <div class="card card-p-0 card-flush p-3 pt-0">
         <div class="p-5 pb-2">
             <h4 class="mb-0">View and manage your NFC Cards</h4>
@@ -91,9 +91,6 @@
                                     {{ optional($nfc_card->nfcData)->last_name }}
                                 </div>
                             </td>
-                            {{-- <td>
-                                        <button class="btn btn-light-primary">94</button>
-                                    </td> --}}
                             <td>
                                 <a href="{{ $nfc_card->nfc_url }}" target="_blank" class="text-primary">
                                     <button class="btn btn-sm btn-info">NFC <i class="fas fa-link ps-2"></i></button>
@@ -148,126 +145,16 @@
                                             Delete
                                         </a>
                                     </div>
-                                    <!--end::Menu item-->
-
-                                    <!--begin::Menu item-->
-                                    {{-- <div class="menu-item px-3">
-                                                <a href="{{ route('user.nfc-card.destroy', $nfc_card->id) }}"
-                                                    class="menu-link px-3 delete">
-                                                    Delete
-                                                </a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-
-
-    @foreach ($nfc_cards as $nfc_card)
-        <div class="modal fade" tabindex="-1" id="message_modal_{{ $nfc_card->id }}">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content position-absolute">
-                    <div class="modal-header">
-                        <h5 class="modal-title">All Messages of your NFC</h5>
-
-                        <!--begin::Close-->
-                        <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal"
-                            aria-label="Close">
-                            <i class="ki-duotone ki-cross fs-2x"><span class="path1"></span><span
-                                    class="path2"></span></i>
-                        </div>
-                        <!--end::Close-->
-                    </div>
-
-                    <div class="modal-body">
-                        <div class="card">
-                            <table
-                                class="table align-middle border rounded table-row-dashed table-striped table-hover  fs-6 g-5"
-                                id="qr_code">
-                                <thead>
-                                    <tr class="text-gray-500 fw-bold fs-7 text-uppercase">
-                                        <th width="10%">NAME </th>
-                                        <th width="15%">Email</th>
-                                        <th width="8%">IP Address</th>
-                                        <th width="57%">Message</th>
-                                        <th width="10%">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="fw-semibold text-gray-600">
-                                    @foreach ($nfc_card->nfcMessages as $nfc_message)
-                                        <tr>
-                                            <td>
-                                                {{ optional($nfc_message)->name }}
-                                            </td>
-                                            <td>
-                                                <a href="mailto:{{ optional($nfc_message)->email }}"
-                                                    target="_blank">{{ optional($nfc_message)->email }}</a>
-
-                                            </td>
-                                            <td>
-                                                {{ optional($nfc_message)->ip_address }}
-                                            </td>
-                                            <td>
-                                                {{ optional($nfc_message)->message }}
-                                            </td>
-                                            <td class="pe-0">
-                                                <a href="#"
-                                                    class="btn btn-light btn-active-light-primary btn-sm"
-                                                    data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end"
-                                                    data-kt-menu-flip="top-end">
-                                                    Actions
-                                                    <span class="svg-icon fs-5 m-0">
-                                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                                            xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
-                                                            height="24px" viewBox="0 0 24 24" version="1.1">
-                                                            <g stroke="none" stroke-width="1" fill="none"
-                                                                fill-rule="evenodd">
-                                                                <polygon points="0 0 24 0 24 24 0 24"></polygon>
-                                                                <path
-                                                                    d="M6.70710678,15.7071068 C6.31658249,16.0976311 5.68341751,16.0976311 5.29289322,15.7071068 C4.90236893,15.3165825 4.90236893,14.6834175 5.29289322,14.2928932 L11.2928932,8.29289322 C11.6714722,7.91431428 12.2810586,7.90106866 12.6757246,8.26284586 L18.6757246,13.7628459 C19.0828436,14.1360383 19.1103465,14.7686056 18.7371541,15.1757246 C18.3639617,15.5828436 17.7313944,15.6103465 17.3242754,15.2371541 L12.0300757,10.3841378 L6.70710678,15.7071068 Z"
-                                                                    fill="currentColor" fill-rule="nonzero"
-                                                                    transform="translate(12.000003, 11.999999) rotate(-180.000000) translate(-12.000003, -11.999999)">
-                                                                </path>
-                                                            </g>
-                                                        </svg>
-                                                    </span>
-                                                </a>
-                                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4"
-                                                    data-kt-menu="true" style="">
-                                                    <!--begin::Menu item-->
-
-                                                    <!--end::Menu item-->
-
-                                                    <!--begin::Menu item-->
-                                                    <div class="menu-item px-3">
-                                                        <a href="{{ route('individual-message.destroy', $nfc_message->id) }}"
-                                                            class="menu-link px-3 delete">
-                                                            Delete
-                                                        </a>
-                                                    </div>
-                                                    <!--end::Menu item-->
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                        {{-- <button type="button" class="btn btn-primary">Save changes</button> --}}
                                 </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
+
 
 <div class="modal fade" tabindex="-1" id="virtual_card_modal_{{ $nfc_card->id }}">
     <div class="modal-dialog modal-dialog-centered modal-lg" style="max-width: 645px !important;">
@@ -1011,4 +898,3 @@
         </div>
     </div>
 </div>
-@endforeach
