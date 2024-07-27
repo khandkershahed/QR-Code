1225e16948eebdbee5804198fce0f9db4ba6ce03
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-07-27 21:15:27 +0600

    26-07-24

--- resources/views/virtualCard/partials/card_preview/card_three.blade.php
+++ resources/views/virtualCard/partials/card_preview/card_three.blade.php
@@ -65,66 +65,6 @@
       position: absolute;
       border-radius: inherit;
     }
-<<<<<<< HEAD
-
-    /* Responsive adjustments */
-    @media (max-width: 1350px) {
-        .text-lg-start {
-            text-align: center !important;
-        }
-    }
-
-    @media (max-width: 1350px) {
-
-        .punch-card-container,
-        .punch-card-container-back {
-            max-width: 100%;
-        }
-
-        .nfc-preview-box {
-            width: 335px;
-            margin: auto;
-            height: auto;
-        }
-    }
-</style>
-
-<div class="row mt-5 nfc-preview-box">
-    <div class="col-12">
-        <div class="punch-card-container mb-2" style="background-image: url('https://i.ibb.co/M9VMCKf/card-5.png');">
-            <div class="row p-5 align-items-center">
-                <!-- Left column for card details -->
-                <div class="col-lg-8 text-lg-start text-start">
-                    {{-- <div>
-                        <img class="punch-card-logo mb-4 card_logo"
-                            src="https://i.ibb.co/vhZg1xp/png-clipart-light-white-line-light-angle-white-removebg-preview.png"
-                            alt="Logo" />
-                    </div> --}}
-                    <div class="text-start content-area font pt-5" style="height: 18vh; margin:0 auto;">
-                        <h4 class="fw-bold mb-0 card_name card_font_color" style="color: #fff">
-                            Nicholas Shelton 3
-                        </h4>
-                        <p class="fw-bold mb-0 card_designation card_font_color">Frontend Developer</p>
-                        <hr class="title-divider mt-2" />
-                        <div>
-                            <p class="fw-bold text-white pt-3 mb-0">
-                                <i class="fas fa-phone"></i> <span class="card_phone card_font_color">015 7661
-                                    4451</span>
-                            </p>
-                            <p class="fw-bold text-white mb-0">
-                                <i class="fas fa-envelope"></i> <span
-                                    class="card_email card_font_color">goflixza@mail.com</span>
-                            </p>
-                        </div>
-                        <div>
-                            <p class="fw-bold mb-0 text-white">
-                                <i class="fas fa-map-marker-alt"></i> <span class="card_address card_font_color">New
-                                    York, United
-                                    State
-                                </span>
-                            </p>
-                        </div>
-=======
     .credit-card-wrap .mk-icon-world-map:before {
       top: 0;
       left: 0;
@@ -274,7 +214,6 @@
                           Khilkhet, Dhaka, Bangladesh
                         </span>
                       </div>
->>>>>>> c0073b6b4be035eec061d5a5617b3202ee9705df
                     </div>
                     <div class="pull-right">
                       <img
