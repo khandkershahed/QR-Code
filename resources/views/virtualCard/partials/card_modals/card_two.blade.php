77bd89275f258a6691d6a061d2f3523c8165a0fa
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-08-18 00:33:39 +0600

    Some changes done

--- resources/views/virtualCard/partials/card_modals/card_two.blade.php
+++ resources/views/virtualCard/partials/card_modals/card_two.blade.php
@@ -196,7 +196,19 @@
             top: 145px;
             z-index: 5;
         }
+        .tempmail{
+            text-align: center;
+            width: 100%;
+            margin: auto;
+            margin-top: -90px;
+        }
     }
+    .tempmail{
+            text-align: center;
+            width: 100%;
+            margin: auto;
+            margin-top: 85px;
+        }
 </style>
 
 <div class="container">
@@ -263,8 +275,8 @@
                         @endif
                     </div>
                     <!-- Front Info -->
-                    <div class="template-two-title pt-5"
-                        style="text-align: center;width: 100%;margin: auto;margin-top: -90px;">
+                    <div class="template-two-title pt-5 tempmail"
+                        style="">
                         <div class="half mt-4">
                             <div class="w-75 mx-auto">
                                 <p class="mb-0 card_email card-two-mail">{{ $nfc_card->card_email }}</p>
--- resources/views/virtualCard/partials/card_preview/card_two.blade.php
+++ resources/views/virtualCard/partials/card_preview/card_two.blade.php
@@ -196,7 +196,19 @@
             top: 145px;
             z-index: 5;
         }
+        .tempmail{
+            text-align: center;
+            width: 100%;
+            margin: auto;
+            margin-top: -90px;
+        }
     }
+    .tempmail{
+            text-align: center;
+            width: 100%;
+            margin: auto;
+            margin-top: 85px;
+        }
 </style>
 
 <div class="container">
@@ -257,8 +269,8 @@
                             src="https://www.goflixza.com/storage/nfc/qrs/NFC7201_nfc_qr.png" alt="">
                     </div>
                     <!-- Front Info -->
-                    <div class="template-two-title pt-5"
-                        style="text-align: center;width: 100%;margin: auto;margin-top: -90px;">
+                    <div class="template-two-title pt-5 tempmail"
+                        style="">
                         <div class="half mt-4">
                             <div class="w-75 mx-auto">
                                 <p class="mb-0 card_email card-two-mail">info@gmail.com</p>
