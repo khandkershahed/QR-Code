4ab075480cf259f924b018947c44db6e55f2c321
shahed141123 <khandkershahed23@gmail.com>
2024-12-15 17:10:12 +0600

    08-12-24

--- resources/views/user/profile/user_plans.blade.php
+++ resources/views/user/profile/user_plans.blade.php
@@ -231,9 +231,10 @@
                                                                                     class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">
                                                                                     {{ $description }}</span>
                                                                                 <i
-                                                                                    class="ki-duotone ki-check-circle fs-1 text-success"><span
-                                                                                        class="path1"></span><span
-                                                                                        class="path2"></span></i>
+                                                                                    class="ki-duotone ki-check-circle fs-1 text-success">
+                                                                                    <span class="path1"></span>
+                                                                                    <span class="path2"></span>
+                                                                                </i>
                                                                             </div>
                                                                         @endforeach
                                                                     </div>
