b3bef89f53515c606cfff3167c0764d43811212c
shahed141123 <khandkershahed23@gmail.com>
2024-12-25 20:15:51 +0600

    18-10-24

--- resources/views/user/pages/bar-code/index.blade.php
+++ resources/views/user/pages/bar-code/index.blade.php
@@ -1,6 +1,20 @@
 <x-app-layout :title="'Bar Code List'">
-    <div class="row">
-        @include('barCode.barcode_partial_index')
+    <div class="col-lg-12">
+        <div class="card rounded-0 mt-4">
+            <div class="card-header p-5 align-items-center rounded-0 bg-info m-0">
+                <div>
+                    <h1 class="mb-0 mt-0 card-title fs-2 text-white">All Bar Code!</h1>
+                    <p class="text-white mt-2 mb-0">View and manage all Bar Code information in this page.</p>
+                </div>
+                <div>
+                    <a href="{{ route('user.barcode.create') }}"
+                        class="btn btn-white btn-active-light-warning text-hover-inverse-white">
+                        <i class="fa-solid fa-user-plus"></i> Add Bar-Code
+                    </a>
+                </div>
+            </div>
+            @include('barCode.barcode_partial_index')
+        </div>
     </div>
 
 </x-app-layout>
