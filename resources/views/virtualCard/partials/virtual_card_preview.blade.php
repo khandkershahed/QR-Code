87e00a9ec2c3508bc634bda5ad46d1147b7044ed
sazeduzzaman <szamansaju@gmail.com>
2025-03-11 23:28:21 +0600

    some changes done

--- resources/views/virtualCard/partials/virtual_card_preview.blade.php
+++ resources/views/virtualCard/partials/virtual_card_preview.blade.php
@@ -33,23 +33,93 @@
     @include('virtualCard.partials.card_preview.card_seven')
 
 </div> --}}
+<style>
+    .nfc-preview-box {
+        width: 500px;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .nfc-preview-box .card-info {
+        position: absolute;
+        bottom: 40px;
+        left: 40px;
+        color: white;
+    }
+
+    .nfc-preview-box .card-logo {
+        position: absolute;
+        top: 30px;
+        left: 30px;
+    }
+
+    .nfc-preview-box .card-logo img {
+        width: 100px;
+    }
+
+    .main-img img {
+        width: 100%;
+        display: block;
+    }
+
+    .nfc-preview-box .card-info h3 {
+        color: transparent;
+        font-size: 25px;
+        font-weight: 800;
+        line-height: 26.25px;
+        margin-bottom: 8px;
+        background-image: linear-gradient(93deg, rgb(156, 156, 156) 2.81%, rgb(244, 244, 244) 29.58%);
+        background-clip: text;
+    }
+
+    .nfc-preview-box .card-info p {
+        color: transparent;
+        font-size: 18px;
+        font-weight: 800;
+        line-height: 26.25px;
+        margin-bottom: 8px;
+        background-image: linear-gradient(93deg, rgb(156, 156, 156) 2.81%, rgb(244, 244, 244) 29.58%);
+        background-clip: text;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .nfc-preview-box {
+            width: 100%;
+        }
+
+        .nfc-preview-box .card-logo img {
+            width: 60px;
+        }
+        .nfc-preview-box .card-info h3{
+            font-size: 18px;
+        }
+        .nfc-preview-box .card-info p{
+            font-size: 16px;
+        }
+    }
+</style>
+
 <div class="card virtual_card nfc-seven-card-frame nfc-seven virtual-card-seven w-100" style="background-color: #ebebeb;">
     <div
         class="pb-3 mb-0 card-design-box d-flex justify-content-center align-items-center position-relative flex-column">
-        <div class="main-img position-relative">
+        <div class="main-img nfc-preview-box">
             <!-- Main Card Image -->
             <img class="img-fluid" id="mainCardImage" src="{{ asset('images/Choosen-card/BlackCardBack-mockup.webp') }}"
                 alt="">
-            <div class="card-logo position-absolute">
-                <!-- Image preview will be displayed here -->
-                <img id="logoPreview" width="50px" height="50px" style="object-fit: contain;"
-                    src="{{ asset('images/logo.png') }}" alt="Card Logo Preview">
+
+            <!-- Logo Positioned on the Image -->
+            <div class="card-logo">
+                <img id="logoPreview" style="object-fit: contain;" src="{{ asset('images/logo.png') }}"
+                    alt="Card Logo Preview">
             </div>
-            <div class="card-info position-absolute">
-                <p class="mb-0 fw-bold">MD BADRODDUJA SAGAR</p>
+
+            <!-- Card Info Placed Below the Image -->
+            <div class="card-info">
+                <h3 class="mb-0 fw-bold">MD BADRODDUJA SAGAR</h3>
                 <p class="mb-0"><small>President & CEO</small></p>
             </div>
         </div>
-        
     </div>
 </div>
